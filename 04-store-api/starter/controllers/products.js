5d1e0a46a3c63e13727f0f4ace4462c2629d5291
Vedant Khedekar <vedant.khedekar18@gmail.com>
2026-02-05 12:58:00 +0530

    final commit for 4

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -8,7 +8,7 @@ const getAllProductsStatic = async (req, res) =>{
 }
 
 const getAllProducts = async (req, res) =>{
-    const { featured,company,name} = req.query
+    const { featured,company,name,sort,feild,numericFilter} = req.query
     const queryObject = {}
     if(featured){
         queryObject.featured = featured === 'true' ? true : false
@@ -19,8 +19,43 @@ const getAllProducts = async (req, res) =>{
     if(name){
         queryObject.name = { $regex : name, $options: 'i'}
     }
-    console.log(queryObject)
-    const products = await product.find(queryObject)
+
+    if(numericFilter){
+        const operatorMap = {
+            '>': '$gt',
+            '>=': '$gte',
+            '=': '$eq', 
+            '<': '$lt',
+            '<=': '$lte',
+        }
+
+        const regEX = /\b(<|>|>=|=|<|<=)\b/g
+        let filters = numericFilter.replace(
+            regEX,
+            (match) => `-${operatorMap[match]}-`
+        )
+        const options = ['price','rating']
+        filters = filters.split(',').forEach((item) =>{
+            const [field, operator, value] = item.split('-')
+            if(options.includes(field)){
+                queryObject[field] = {[operator]: Number(value)}
+            }
+        })
+    }
+    let results = product.find(queryObject)
+    if(sort){
+        const sortList = sort.split(',').join(" ")
+        results = results.sort(sortList)
+    }
+    else{
+        results = results.sort('createdAt')
+    }
+    if(feild){
+        const feildList = feild.split(',').join(" ")
+        results = results.select(feildList)
+    }
+
+    const products = await results 
     res.status(200).json({products,nbHits: products.length})
 }
 
