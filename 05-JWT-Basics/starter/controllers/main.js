20ee154ecbe4463cb50ffc389b517c88d3cc0150
Vedant Khedekar <vedant.khedekar18@gmail.com>
2026-02-10 16:00:53 +0530

    project 5

---
+++ 05-JWT-Basics/starter/controllers/main.js
@@ -0,0 +1,26 @@
+const CustomAPIError = require('../errors/custom-error')
+const jwt = require('jsonwebtoken')
+
+const login = async (req,res) =>{
+    const {username,password} = req.body
+
+    if(!username || !password){
+        throw new CustomAPIError('Please provide Username and Password',401)
+    }
+
+    const id = new Date().getDate()
+
+    const token = jwt.sign({id,username},process.env.JWT_SECRET,{expiresIn:'30d'})
+    console.log(username,password)
+    res.status(200).json({msg:`Hello ${username}`,token})
+}
+
+const dashboard = async (req,res) =>{
+    const luckyNumber = Math.floor(Math.random()*100)
+    res.status(200).json({msg:`Hello, Vedant Khedekar`, secret:`Here is your authorized data,your lucly number is ${luckyNumber}`})
+}
+
+module.exports = {
+    login,
+    dashboard
+}
